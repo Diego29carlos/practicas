4fd1cc165871032832ee6543ad3b8ed927c92257
diego29carlos <juan29-diego@hotmail.com>
2020-02-13 14:30:31 -0300

    practicando

---
+++ script.js
@@ -0,0 +1,14 @@
+function datos(obj) {
+    for (let [key,value]of Object.entries (obj)){
+        console.log(key + ':' + value);
+    }
+}
+
+
+const obj = {
+
+    name:'fulano',
+    surname:'garcia',
+    age:'20'
+};
+ 
