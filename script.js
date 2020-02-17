20fcd3c46b7790f60e65b451041acdb192f35175
diego29carlos <vivianaachoque@gmail.com>
2020-02-17 14:07:30 -0300

    agua

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function datos(obj) {
-    for (let [key,value]of Object.entries (obj)){
-        console.log(key + ':' + value);
+const array1 = [1,2,3,]
+const array2 = [1,2,3]
+const array3 = [1,2,3]
+
+function mergeArrays(...array){
+
+    let resultado = [];
+
+    for(let i = 0; i<mergeArrays.length; i++){
+
+        resultado = resultado.concat(array[i]);
     }
+
+    return resultado;
 }
-
-
-const obj = {
-
-    name:'fulano',
-    surname:'garcia',
-    age:'20'
-};
- 
