d90002528d8145061e2177c7227db1a02e9247d2
diego29carlos <juan29-diego@hotmail.com>
2020-02-21 16:27:52 -0300

    tierra

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-const array1 = [1,2,3,]
-const array2 = [1,2,3]
-const array3 = [1,2,3]
-
-function mergeArrays(...array){
-
-    let resultado = [];
-
-    for(let i = 0; i<mergeArrays.length; i++){
-
-        resultado = resultado.concat(array[i]);
-    }
-
-    return resultado;
-}
+function esAnagrama(palabra, posibleAnagrama){
+    //Si desde un principio son iguales, regresamos false; ya que no tienen orden distinto
+    if(palabra.toLowerCase() === posibleAnagrama.toLowerCase()) return false;
+    
+    return palabra.toLowerCase().split("").sort().join("") === posibleAnagrama.toLowerCase().split("").sort().join("");
+  }
+  
+  var palabrasYPosiblesAnagramas = [
+      {
+        palabra: "Hola",
+        posibleAnagrama: "Aloh"
+      },
+      {
+        palabra: "Frase",
+        posibleAnagrama: "Fresa"
+      },
+      {
+        palabra: "Escandalizar",
+        posibleAnagrama: "Zascandilear"
+      },
+      {
+        palabra: "Conservadora",
+        posibleAnagrama: "Conversadora"
+      },
+      {
+        palabra: "Perro",
+        posibleAnagrama: "Gato"
+      },
+      {
+        palabra: "Maggie",
+        posibleAnagrama: "Maggie"
+      },
+    ];
+    
+  for(var x = palabrasYPosiblesAnagramas.length - 1; x >= 0; x--){
+    var palabra = palabrasYPosiblesAnagramas[x].palabra,
+      posibleAnagrama = palabrasYPosiblesAnagramas[x].posibleAnagrama,
+      resultado = esAnagrama(palabra, posibleAnagrama);
+      
+    console.log("¿%s es anagrama de %s? %s", posibleAnagrama, palabra, resultado);
+  }
